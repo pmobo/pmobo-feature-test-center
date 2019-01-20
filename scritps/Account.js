90cf0af7b8f617b7b7bc65e85802e28a813cd1fc
Guilherme Soster <GSoster@users.noreply.github.com>
2019-01-20 20:06:08 -0200

    implmeneted initial structure to do account requests

---
+++ scritps/Account.js
@@ -0,0 +1,7 @@
+"use strict";
+
+var enpoints = {};
+endpoints.baseUrl = "https://pmobo2.azurewebsites.net";
+endpoints.Account = {};
+endpoints.Account.Register = "/api/Account/Register";
+endpoints.Account.TokenRequest = "/token";
