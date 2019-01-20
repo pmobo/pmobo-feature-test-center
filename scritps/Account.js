513b046783cb3acbb15116159e5c701b5a19ccab
Guilherme Soster <GSoster@users.noreply.github.com>
2019-01-20 21:54:19 -0200

    implemented simple visual sample of how things should be

--- scritps/Account.js
+++ scritps/Account.js
@@ -1,7 +1,8 @@
 "use strict";
 
-var enpoints = {};
-endpoints.baseUrl = "https://pmobo2.azurewebsites.net";
 endpoints.Account = {};
 endpoints.Account.Register = "/api/Account/Register";
 endpoints.Account.TokenRequest = "/token";
+
+
+
---
+++ scritps/Global.js
@@ -0,0 +1,3 @@
+"use strict";
+var enpoints = {};
+endpoints.baseUrl = "https://pmobo2.azurewebsites.net";
